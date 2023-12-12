67ee36dc66d56136bd2a9d07ca856e23698756f6
Matjaž Drolc <mdrolc@gmail.com>
2023-12-12 15:40:06 +0100

    rename

--- src/bin/day_11_cosmic_expansion.rs
+++ src/bin/day_11_cosmic_expansion.rs
@@ -5,31 +5,7 @@ use std::env;
 use std::fs;
 use itertools::Itertools;
 
-fn expand_cosmos(c: &mut Vec<Vec<u8>>) {
-    // expand lines
-    let mut y = 0usize;
-    while y < c.len() {
-        if c[y].iter().all(|&v| v == b'.') {
-            c.insert(y, c[y].clone());
-            y += 2;
-        } else {
-            y += 1;
-        }
-    }
-
-    // expand columns
-    let mut x = 0usize;
-    while x < c[0].len() {
-        if c.iter().all(|l| l[x] == b'.') {
-            c.iter_mut().for_each(|f| f.insert(x, b'.'));
-            x += 2;
-        } else {
-            x += 1;
-        }
-    }
-}
-
-fn expand_universe_v2(c: &Vec<Vec<u8>>, expansion_factor: usize) -> (Vec<usize>, Vec<usize>) {
+fn expand_cosmos(c: &Vec<Vec<u8>>, expansion_factor: usize) -> (Vec<usize>, Vec<usize>) {
     let mut x_mapping = vec![0; c[0].len()];
     let mut y_mapping = vec![0; c.len()];
 
@@ -97,12 +73,12 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Cannot read file");
     let cosmos = contents.lines().map(|f| f.bytes().into_iter().collect_vec()).collect_vec();
 
-    let position_mapping_1 = expand_universe_v2(&cosmos, 2);
+    let position_mapping_1 = expand_cosmos(&cosmos, 2);
     let galaxies_1 = find_galaxies(&cosmos, position_mapping_1);
     let paths_sum_part_1 = sum_distances(galaxies_1);
     println!("Sum of path lengths [part 1]: {}", paths_sum_part_1);
 
-    let position_mapping_2 = expand_universe_v2(&cosmos, 1_000_000);
+    let position_mapping_2 = expand_cosmos(&cosmos, 1_000_000);
     let galaxies_2 = find_galaxies(&cosmos, position_mapping_2);
     let paths_sum_part_2 = sum_distances(galaxies_2);
     println!("Sum of path lengths [part 2]: {}", paths_sum_part_2);
