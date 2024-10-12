7ee11659937cef801f243cc5f46fb386f7b171bd
Matjaž Drolc <mdrolc@gmail.com>
2024-10-12 18:03:00 +0200

    Solved Day 14 in Kotlin

---
+++ kotlin/src/test/kotlin/Day14Test.kt
@@ -0,0 +1,87 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+
+class Day14Test {
+    @Test
+    fun testSlideNorth() {
+        var input = """O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....""".trimMargin();
+        var expectedOutput = """OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#....""".trimMargin();
+        assertEquals(expectedOutput, toString(slideNorth(fromString(input))));
+    }
+
+    @Test
+    fun testRotateCW() {
+        var input = """
+            |123
+            |456
+            |789""".trimMargin();
+        var expectedOutput = """
+            |741
+            |852
+            |963""".trimMargin()
+        assertEquals(expectedOutput, toString(rotateCW(fromString(input))));
+    }
+
+    @Test
+    fun testQuarterCycle() {
+        var input = """O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+""".trimMargin();
+        var expectedOutputFullCycle = """.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+""".trimMargin();
+        assertEquals(
+            expectedOutputFullCycle,
+            toString(quarterCycle(quarterCycle(quarterCycle(quarterCycle(fromString(input))))))
+        );
+    }
+
+    @Test
+    fun testSolveLine() {
+        assertAll(
+            { assertEquals(1, solveLine(Line("", listOf()))) },
+            { assertEquals(1, solveLine(Line("???.###", listOf(1, 1, 3)))) },
+            { assertEquals(4, solveLine(Line(".??..??...?##.", listOf(1, 1, 3)))) },
+            { assertEquals(1, solveLine(Line("?#?#?#?#?#?#?#?", listOf(1, 3, 1, 6)))) },
+            { assertEquals(1, solveLine(Line("????.#...#...", listOf(4, 1, 1)))) },
+            { assertEquals(4, solveLine(Line("????.######..#####.", listOf(1, 6, 5)))) },
+            { assertEquals(10, solveLine(Line("?###????????", listOf(3, 2, 1)))) }
+        )
+    }
+}
